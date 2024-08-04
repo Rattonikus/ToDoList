15f3912c50d14a6823d6cd125a5bef0391f09bc0
Rattonikus <71366308+Rattonikus@users.noreply.github.com>
2024-08-03 21:43:34 -0600

    Scrollable button panel

--- src/view/todo/TodoPanel.java
+++ src/view/todo/TodoPanel.java
@@ -2,6 +2,7 @@ package view.todo;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import java.awt.LayoutManager;
@@ -11,6 +12,7 @@ import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.border.Border;
 
 public class TodoPanel extends JPanel 
 {
@@ -32,6 +34,7 @@ public class TodoPanel extends JPanel
 		this.listNames = new ArrayList<String>();
 		this.scroller = new JScrollPane(buttonPanel, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 		listNames.add("Task1");
+		this.scroller.setPreferredSize(new Dimension(100, 100));
 		this.list = new JButton(listNames.get(0));
 
 		this.buttonLayout = new BoxLayout(buttonPanel, BoxLayout.Y_AXIS);
@@ -50,8 +53,9 @@ public class TodoPanel extends JPanel
 	{
 		
 		this.buttonPanel.add(list);
+		this.list.setAlignmentX(CENTER_ALIGNMENT);
 		this.buttonPanel.setBackground(Color.CYAN);
-		this.add(scroller);
+		this.add(scroller, BorderLayout.NORTH);
 		this.validate();
 
 	}
@@ -71,7 +75,9 @@ public class TodoPanel extends JPanel
 	private void changeList()
 	{
 		listNames.add("name3");
-		this.buttonPanel.add(new JButton(listNames.get(listNames.size() - 1)));
+		JButton newButton = new JButton(listNames.get(listNames.size() - 1));
+		this.buttonPanel.add(newButton);
+		newButton.setAlignmentX(CENTER_ALIGNMENT);
 		System.out.println(listNames.size());
 		this.validate();
 	}
