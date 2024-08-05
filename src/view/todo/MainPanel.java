8499a7d3d252b1b2d784d56c283b611d99528d73
Rattonikus <71366308+Rattonikus@users.noreply.github.com>
2024-08-04 20:48:17 -0600

    Button Spacing and panel corrections

--- src/view/todo/MainPanel.java
+++ src/view/todo/MainPanel.java
@@ -4,14 +4,20 @@ import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.border.Border;
+import javax.swing.border.EmptyBorder;
 
 public class MainPanel extends JPanel
 {
 
     private JPanel topPanel;
+    private JPanel bottomPanel;
+    private JPanel leftSpacer; 
+    private JPanel rightSpacer;
+
     private JButton addList;
     private JButton editList;
 
@@ -24,27 +30,49 @@ public class MainPanel extends JPanel
         this.topPanel = new JPanel();
         this.topPanel.setPreferredSize(new Dimension(100, 100));
 
-
+        //Top Panel Buttons
         this.addList = new JButton("Add List");
         this.editList = new JButton("Edit List");
 
-
+        //Spacer Panels
+        this.bottomPanel = new JPanel();
+        this.leftSpacer = new JPanel();
+        this.rightSpacer = new JPanel();
+        
+        this.bottomPanel.setPreferredSize(new Dimension(100, 100));
+        this.leftSpacer.setPreferredSize(new Dimension(100, 100));
+        this.rightSpacer.setPreferredSize(new Dimension(100, 100));
+        
+        //Main Panel
         this.setLayout(new BorderLayout());
-        this.topPanel.setLayout(null);
-
+        this.topPanel.setLayout(new BorderLayout());
+        
+        //Function Calls
         setupPanel();
 
     }
 
     private void setupPanel()
     {
-        //Top panel
-        this.topPanel.add(addList);
-        this.topPanel.add(editList);
-        this.addList.setBounds(10, 10, 100, 80);
-        this.editList.setBounds(120, 10, 100, 80);
-
+        //Top panel buttons 
+        this.topPanel.add(addList, BorderLayout.WEST);
+        this.topPanel.add(editList, BorderLayout.EAST);
+        this.addList.setPreferredSize(new Dimension(100,100));
+        this.editList.setPreferredSize(new Dimension(100,100));
+    
         this.topPanel.setBackground(Color.gray);
+
+        //Border between buttons and edge of topPanel
+        this.topPanel.setBorder(new EmptyBorder(10,10,10,10));
+
+        //Spacer Panels
+        this.add(bottomPanel, BorderLayout.SOUTH);
+        this.add(leftSpacer, BorderLayout.WEST);
+        this.add(rightSpacer, BorderLayout.EAST);
+
+        this.bottomPanel.setBackground(Color.green);
+        this.leftSpacer.setBackground(Color.red);
+        this.rightSpacer.setBackground(Color.blue);
 
         //Main Panel
         this.add(topPanel, BorderLayout.NORTH);
--- src/view/todo/TodoFrame.java
+++ src/view/todo/TodoFrame.java
@@ -1,6 +1,7 @@
 package view.todo;
 
 import java.awt.BorderLayout;
+import java.awt.Dimension;
 
 import javax.swing.JFrame;
 
@@ -20,7 +21,8 @@ public class TodoFrame extends JFrame
 	private void setupFrame()
 	{
 		this.setContentPane(mainPanel);
-		 this.setSize(1200, 900);
+		 this.setSize(500, 900);
+		 this.setMinimumSize(new Dimension(300, 500));
 		 this.setTitle("Java Todo");
 		 this.setResizable(true);
 		 
